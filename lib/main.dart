38fa10bd3db4f0c88d7f20efda4b551dad6da3e9
k <kiraygm.12@gmail.com>
2026-08-03 18:43:08 +0800

    dawn

--- lib/main.dart
+++ lib/main.dart
@@ -7,12 +7,24 @@ void main() {
       appBar: AppBar(
         title: Text('my first app'),
         centerTitle: true,
+        backgroundColor: Colors.red[600],
       ),
       body: Center(
-        child: Text('hello ninjas'),
+        child: Text(
+          'Hello Ninjas',
+          style: TextStyle(
+            fontSize: 20,
+            fontWeight: FontWeight.bold,
+            letterSpacing: 2.0,
+            color: Colors.grey[600],
+            fontFamily: "OpenSans",
+
+          ),
+        ),
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: null,
+        backgroundColor: Colors.red[600],
         child: Text('Click'),
       ),
     ),
