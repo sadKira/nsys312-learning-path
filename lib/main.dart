3217e5e02d930a417d3014c5d93e2b1ecba46789
k <kiraygm.12@gmail.com>
2026-08-07 17:26:57 +0800

    dawn

--- lib/main.dart
+++ lib/main.dart
@@ -1,33 +1,44 @@
 import 'package:flutter/material.dart';
 
+// Entry Point
 void main() {
-  runApp(MaterialApp(
+  runApp(Home());
+}
 
-    home: Scaffold(
-      appBar: AppBar(
-        title: Text('my first app'),
-        centerTitle: true,
-        backgroundColor: Colors.red[600],
-      ),
-      body: Center(
-        child: Text(
-          'Hello Ninjas',
-          style: TextStyle(
-            fontSize: 20,
-            fontWeight: FontWeight.bold,
-            letterSpacing: 2.0,
-            color: Colors.grey[600],
-            fontFamily: "OpenSans",
+// Root Widget
+class Home extends StatelessWidget {
 
+  // Constructor
+  const Home({super.key});
+
+  // Build Method
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('my first app'),
+          centerTitle: true,
+          backgroundColor: Colors.red[600],
+        ),
+        body: Center(
+          child: Text(
+            'Hello Ninjas',
+            style: TextStyle(
+              fontSize: 20,
+              fontWeight: FontWeight.bold,
+              letterSpacing: 2.0,
+              color: Colors.grey[600],
+              fontFamily: "OpenSans",
+            ),
           ),
         ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: null,
+          backgroundColor: Colors.red[600],
+          child: Text('Click'),
+        ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: null,
-        backgroundColor: Colors.red[600],
-        child: Text('Click'),
-      ),
-    ),
-
-  ));
+    );
+  }
 }
