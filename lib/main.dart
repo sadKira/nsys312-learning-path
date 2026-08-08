3a8cad4236c29feb584116272b3fbddcb4381acb
k <kiraygm.12@gmail.com>
2026-08-08 13:33:59 +0800

    Progress

--- lib/main.dart
+++ lib/main.dart
@@ -7,7 +7,6 @@ void main() {
 
 // Root Widget
 class Home extends StatelessWidget {
-
   // Constructor
   const Home({super.key});
 
@@ -22,7 +21,10 @@ class Home extends StatelessWidget {
           backgroundColor: Colors.red[600],
         ),
         body: Center(
-          child: Image.asset('images/Jun2026.png'),
+          child: ElevatedButton(
+            onPressed: () {}, 
+            child: Text('Click Me'),
+            ),
         ),
         floatingActionButton: FloatingActionButton(
           onPressed: null,
