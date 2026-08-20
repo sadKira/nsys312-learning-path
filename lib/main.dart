fbd0600669c7974b7d61834a72395bcd6729224f
k <kiraygm.12@gmail.com>
2026-08-20 19:28:42 +0800

    Updated comment content

--- lib/main.dart
+++ lib/main.dart
@@ -10,18 +10,23 @@ void main() {
     // A Scaffold widget is a wrapper for other layout widgets
     // Applicable with the material design system
     home: Scaffold(
+
+      // Added an application menu bar using the AppBar widget
       appBar: AppBar(
-        title: Text('my first app'),
-        centerTitle: true,
+        title: Text('my first app'), // Adding text inside the AppBard widget
+        centerTitle: true, // Center the title property
       ),
+
+      // Added center widget for centered content
       body: Center(
-        child: Text('hello ninjas'),
+        child: Text('hello ninjas'), // Used Text widget for text
       ),
+
+      // Added floating button
       floatingActionButton: FloatingActionButton(
-        onPressed: null,
-        child: Text('Click'),
+        onPressed: null, // This property is need when using buttons
+        child: Text('Click'), // Adding text inside the button
       ),
     ),
-
   ));
 }
