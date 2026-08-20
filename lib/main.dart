a92c7fc6f095b275db9347f549086b445f49e42f
k <kiraygm.12@gmail.com>
2026-08-20 18:22:11 +0800

    Added comments

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
 
+// Entry point
 void main() {
   runApp(MaterialApp(
 
+    // A Scaffold widget is a wrapper for other layout widgets
+    // Applicable with the material design system
     home: Scaffold(
       appBar: AppBar(
         title: Text('my first app'),
