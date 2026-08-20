1675e7b84e0fc6608a411c353389394b700fe741
k <kiraygm.12@gmail.com>
2026-08-20 19:25:35 +0800

    Added comments

--- lib/main.dart
+++ lib/main.dart
@@ -1,33 +1,48 @@
+// Lesson 2 is about the utilization
+// of colours & fonts (custom fonts).
+// Custom fonts are stored in the assets\fonts folder
+
 import 'package:flutter/material.dart';
 
+// Entry point
 void main() {
   runApp(MaterialApp(
 
+    // Scaffold widget that wraps all other widgets
     home: Scaffold(
+
+      // AppBar widget
       appBar: AppBar(
         title: Text('my first app'),
         centerTitle: true,
-        backgroundColor: Colors.red[600],
+        backgroundColor: Colors.red[600], // Utilizing material's Colors class
       ),
+
+      // Center widget
       body: Center(
+
+        // Text widget
         child: Text(
           'Hello Ninjas',
+
+          // Added a TextStyle widget for styling text
           style: TextStyle(
             fontSize: 20,
             fontWeight: FontWeight.bold,
             letterSpacing: 2.0,
             color: Colors.grey[600],
-            fontFamily: "OpenSans",
+            fontFamily: "OpenSans", // Using a custom font (pubspec.yaml)
 
           ),
         ),
       ),
+
+      // Added floating button
       floatingActionButton: FloatingActionButton(
-        onPressed: null,
+        onPressed: null, // This property is required when using buttons
         backgroundColor: Colors.red[600],
         child: Text('Click'),
       ),
     ),
-
   ));
 }
