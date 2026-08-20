8b4af6a7a0b54a36d21df74f013e1248634bd661
k <kiraygm.12@gmail.com>
2026-08-20 19:42:24 +0800

    Added comments

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,16 @@
+// Lesson 4 is about using custom images and images
+// obtained online with different approaches.
+
+// Images are still stored in the assets folder alongside
+// the fonts.
+
+// NOTE: Custom images must be declared in the pubspec.yaml file
+
 import 'package:flutter/material.dart';
 
 // Entry Point
 void main() {
-  runApp(Home());
+  runApp(Home()); // Calling Root Widget
 }
 
 // Root Widget
@@ -22,6 +30,28 @@ class Home extends StatelessWidget {
           backgroundColor: Colors.red[600],
         ),
         body: Center(
+
+          // Approach 1 - Online Image
+          // This approach is for using online images
+          //
+          // child: Image(
+          //  image: NetworkImage('image_url'),
+          // ),
+
+          // Approach 1 - Custom Image
+          // This approach uses a custom image using the Image Widget
+          //
+          // child: Image(
+          //  image: AssetImage('images/mkd.jpg'),
+          // ),
+
+          // Approach 2 - Online Image
+          // This approach is for efficiency using online images
+          //
+          // child: Image.Network('image_url'),
+
+          // Approach 2 - Custom Image
+          // This approach is an efficient approach when using custom images
           child: Image.asset('images/Jun2026.png'),
         ),
         floatingActionButton: FloatingActionButton(
